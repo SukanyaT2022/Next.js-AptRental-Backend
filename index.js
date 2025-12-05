7b80af09bbccc7ed9887d55249b19a5ba59656ff
TiparpronS. <96738451+SukanyaT2022@users.noreply.github.com>
2025-12-05 11:50:36 -0500

    add controllers

--- index.js
+++ index.js
@@ -3,26 +3,34 @@ const express = require("express")
 const app = express()
 const PORT = 4000
 
+app.use(express.json())
+
 app.get("/",()=>{
     res.send("Server is running on port 4000")
 })
 
-app.get('/data', (req, res) => {
-    res.json([
-                {
-                    "id": 1,
-                    "title": "Item One",
-                    "price": 199
-                },
-                {
-                    "id": 2,
-                    "title": "Item Two",
-                    "price": 299
-                }
-            ]
-        )
+// Get route -send from front end to backend
+// req => This always comes from frontend
+// res => This is something we send from backend
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    if(username == "yoon" && password == 12345){
+        res.json({
+            "status": true,
+            "message": "Logged in successfully"
+    })
+    }else{
+        res.json({
+            "status": false,
+            "message": "Invalid username or password"
+    })
+    }
 })
 
 app.listen(PORT, (req, res)=>{
     console.log("Server is running")
 })
+
+// There are four types of api routes GET, POST, UPDATE, DELETE
